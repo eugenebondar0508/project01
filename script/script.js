28e4bc6b074b7582c9c0c639a7beb18f86c81ffb
eugenebondar0508 <larisa-bondar1@ukr.net>
2021-05-28 17:06:52 +0300

    Modified script.js file

--- script/script.js
+++ script/script.js
@@ -1,11 +1,18 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money  = 1000;
+let income = 'freelance';
+let addExpenses ='internet, taxi, communal';
+let deposit = true;
+let mission = 10000;
+let period = 5;
 
 
+console.log(typeof income);
+console.log(typeof money);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log( `Период равен ${period} месяцев. Цель заработать ${10000} $` );
+console.log(addExpenses.toLocaleUpperCase().split(''));
 
-alert('hello');
-console.log('hello');
+let budgetDay = money / 30;
+console.log(`Бюджет на день: ${Math.floor(budgetDay)}$`);
+
