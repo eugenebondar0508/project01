fb27fffaab76d550d50c0eb821d2aa4d4bfc54bb
eugenebondar0508 <larisa-bondar1@ukr.net>
2021-05-31 13:54:44 +0300

    Corected mistakes

--- script/script.js
+++ script/script.js
@@ -10,8 +10,8 @@ console.log(typeof income);
 console.log(typeof money);
 console.log(typeof deposit);
 console.log(addExpenses.length);
-console.log( `Период равен ${period} месяцев. Цель заработать ${10000} $` );
-console.log(addExpenses.toLocaleUpperCase().split(''));
+console.log( `Период равен ${period} месяцев. Цель заработать ${mission} $` );
+console.log(addExpenses.toLowerCase().split(','));
 
 let budgetDay = money / 30;
 console.log(`Бюджет на день: ${Math.floor(budgetDay)}$`);
