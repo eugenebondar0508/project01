4a429f23bb82e114a308bd1e19877a1e9427b0db
eugenebondar0508 <larisa-bondar1@ukr.net>
2021-06-14 22:55:24 +0300

    fixed anather bug

--- script/script.js
+++ script/script.js
@@ -107,9 +107,19 @@ let isNumber = function(n){
         } else {
             calculate.disabled = false;
         };
-        return appData = [];
-
-        console.log(this);
+        this.income = {};
+        this.incomeMonth =0;
+        this.addIncome =[];
+        this.expenses = {};
+        this.addExpenses =[];
+        this.deposit = false;
+        this.percentDeposit =0;
+        this.moneyDeposit =0;
+        this.budget = 0;
+        this.budgetDay =0;
+        this.budgetMonth  =0;
+        this.expensesMonth =0;
+        this.sum =0;
      },
      showResult: function(){
          budgetMonthValue.value = this.budgetMonth;
